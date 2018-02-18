2019b47a943cff475554f8ed2ea619c5cac73ffd
metaphors <2360514681@qq.com>
2018-02-18 17:44:18 +0800

    feat(pages edit-password): page edit-password DONE

--- src/pages/edit-password/edit-password.ts
+++ src/pages/edit-password/edit-password.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+
+import * as wilddog from "wilddog";
 
 /**
  * Generated class for the EditPasswordPage page.
@@ -14,6 +16,35 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
   templateUrl: 'edit-password.html',
 })
 export class EditPasswordPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  email: string;
+  edit: { password: string, verification: string };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
+    this.edit = {password: '', verification: ''};
+  }
+
+  onUpdateUserPassword() {
+    if (this.edit.password === '' || this.edit.verification === '') {
+      this.presentToast('输入信息不能为空');
+    } else if (this.edit.password !== this.edit.verification) {
+      this.presentToast('两次输入的密码不一致');
+    } else if (this.edit.password.length < 6 || this.edit.password.length > 32) {
+      this.presentToast('密码长度必须在 6 到 32 位之间');
+    } else {
+      wilddog.auth().currentUser.updatePassword(this.edit.password).then(() => {
+        this.navCtrl.pop().then(value => {
+          return value;
+        });
+      }, error => {
+        this.presentToast(error.name + ': ' + error.message);
+      });
+    }
+  }
+
+  presentToast(message: string) {
+    let toast = this.toastCtrl.create({message: message, duration: 1500, dismissOnPageChange: true});
+    toast.present().then(value => {
+      return value;
+    });
   }
 }
